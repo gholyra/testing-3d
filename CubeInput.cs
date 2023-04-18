c2dfe2abbe50056a26d5f80f47e556c523aa2e0d
Tiago Marchetti Lyra <tiago.mlyra@gmail.com>
2023-04-17 22:20:13 -0300

    1º Commit do Projeto

---
+++ CubeInput.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInput : MonoBehaviour
+{
+
+    private Transform transform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        //transform.position = new Vector3(0, 1, 1);
+        transform.Translate(new Vector3(0, 8, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
